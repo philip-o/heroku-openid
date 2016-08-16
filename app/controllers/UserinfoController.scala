0d55c1fa09e3b315ec49b7066ac6e5c0f59782ca
Philip Ogunleye <Philip.Ogunleye@mdtpcollab.digital.hmrc.gov.uk>
2016-08-16 15:21:19 +0100

    Fixed userinfo endpoint

--- app/controllers/UserinfoController.scala
+++ app/controllers/UserinfoController.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import play.api.Logger
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 import play.api.mvc.Results.{BadRequest, Ok}
@@ -9,17 +10,21 @@ trait UserinfoController extends Controller {
 
   def userinfo = Action {
     implicit request =>
+
       var params : Map[String,Seq[String]] = Map("error" -> Seq("invalid_token"))
+      Logger.info(s"Headers: ${request.headers.headers}")
       request.headers.headers.filter(obj => obj._1.equalsIgnoreCase("Authorization")).headOption match {
         case Some(header) =>
           println(s"Authorization header value: ${header._2}")
           OpenIDConnectUtil.users.get(header._2.replace("Authorization ","")) match {
-          case None => params += ("error_desription" -> Seq("Invalid token"))
+          case None => params += ("error_description" -> Seq("Invalid token"))
+            Logger.info(s"Invalid token")
             BadRequest(params)
           case Some(user) => Ok(Json.parse(s"""{\"user\":\"${user.username}\",\"email\":\"${user.username}@test.com\"}"""))
         }
         case None =>
-          params += ("error_desription" -> Seq("No authorisation header provided"))
+          params += ("error_description" -> Seq("No authorisation header provided"))
+          Logger.info(s"No authorisation header provided")
           BadRequest(params)
       }
   }
