9336f3535625db5b0a774ee6a136e8740e791976
Philip Ogunleye <Philip.Ogunleye@mdtpcollab.digital.hmrc.gov.uk>
2016-08-16 14:40:58 +0100

    token endpoint log statements

--- app/controllers/TokenController.scala
+++ app/controllers/TokenController.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import models.{TokenErrorResponse, TokenSuccessResponse}
+import play.api.Logger
 import play.api.libs.json.Json
 import play.api.mvc.Results._
 import play.api.mvc.{Action, AnyContent, Request}
@@ -10,11 +11,13 @@ trait TokenEndpointController {
 
   def process() = Action {
     implicit request =>
+      Logger.info("Token request received")
       val grant_type = extractParam("grant_type")
       val code = extractParam("code")
       val redirect_uri = extractParam("redirect_uri")
 
       //validate redirect_uri is the same as the original request
+      Logger.info("Extracted params")
       val resp = grant_type match {
         case "authorization_code" => createResponse(code, redirect_uri)
         case _ => BadRequest(Json.toJson(TokenErrorResponse("invalid_tag")))
@@ -27,10 +30,15 @@ trait TokenEndpointController {
   }
 
   private def createResponse(authCode : String, redirect_uri : String)(implicit request: Request[AnyContent]) = {
+    Logger.info("Creating response")
     val option = OpenIDConnectUtil.users.remove(authCode)
     option match {
-      case None => BadRequest(Json.toJson(TokenErrorResponse("invalid_request")))
-      case Some(found) => val token = TokenSuccessResponse(id_token = OpenIDConnectUtil.createIDToken(found))
+      case None =>
+        Logger.info("User not found")
+        BadRequest(Json.toJson(TokenErrorResponse("invalid_request")))
+      case Some(found) =>
+        Logger.info("User found")
+        val token = TokenSuccessResponse(id_token = OpenIDConnectUtil.createIDToken(found))
         OpenIDConnectUtil.users.put(token.access_token,found)
         Ok(Json.toJson(token))
     }
